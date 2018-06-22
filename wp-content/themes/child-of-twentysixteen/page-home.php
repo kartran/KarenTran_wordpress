cffe90efc9b5636a13a63893f8d63fffd9533a1a
karentran <karen.tran913@hotmail.com>
2018-06-22 16:27:51 -0400

    made changes to php files, removed side bar and title on home page

--- wp-content/themes/child-of-twentysixteen/page-home.php
+++ wp-content/themes/child-of-twentysixteen/page-home.php
@@ -15,15 +15,12 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
-
-	<?php echo "This is a single post page"?>
-
 		<?php
 		// Start the loop.
 		while ( have_posts() ) : the_post();
 
 			// Include the page content template.
-			get_template_part( 'template-parts/content', 'page' );
+			get_template_part( 'template-parts/content', 'homepage' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) {
@@ -40,5 +37,5 @@ get_header(); ?>
 
 </div><!-- .content-area -->
 
-<?php //get_sidebar(); ?> <!--commenting out the sidebar on the home page -->
+<?php //get_sidebar(); ?>
 <?php get_footer(); ?>
